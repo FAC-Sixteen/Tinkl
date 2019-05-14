7ed0a7822f02f85116a2240ce4d53d88cfc40767
Rohan Shah Summers <rohan_shah_summers@yahoo.co.uk>
2019-05-14 18:03:02 +0100

    add html for location page relates #5

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -4,7 +4,9 @@ const express = require('express');
 const router = express.Router();
 
 const home = require('./home');
+const location = require('./location')
 
 router.get('/', home.get);
+router.get('/location', location.get)
 
 module.exports = router;
