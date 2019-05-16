cd62a351debb7718275d1863a272655d3b2b21ce
Joko Sanyang <jo.san@live.co.uk>
2019-05-16 12:44:44 +0100

    Handle errors for geolocation feature Relates #22 #5

--- public/dom.js
+++ public/dom.js
@@ -1,9 +1,10 @@
 const locateMe = document.querySelector('.button__location');
+const alert = document.querySelector('.location__alert');
 
 const geolocate = () => {
-  if(navigator.geolocation){
+  if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(pos => {
-      const url = `/geolocation?long=${pos.coords.longitude}&lat=${pos.coords.latitude}`
+      const url = `/geolocation?long=${pos.coords.longitude}&lat=${pos.coords.latitude}`;
       fetch(url)
         .then(res => {
           window.location.href = '/filter';
@@ -11,22 +12,23 @@ const geolocate = () => {
         .catch(err => {
           console.log('error', err);
         })
-    }, geoError, {timout: 5000})
+    }, geoError, {timeout: 5000})
   } else {
-    //browser doesn't have geolocation
+    alert.textContent = 'Sorry, your browser is not compatible with Use My Location.'
+    locateMe.disabled = true; 
   }
 };
 
 const geoError = error => {
   switch (error.code) {
     case 3:
-      //request timeout
+      alert.textContent = 'Oops, the request timed out.';
       break;
     case 2:
-      //can't get data
+      alert.textContent = 'Oh dear, looks like your location is unavailable at the moment.';
       break;
     case 1:
-      //permission denied
+      alert.textContent = 'Please allow location permissions to use this feature.';
       break;
   }
 }
