f8c4f827ece6dea44f329b1cd106dbfba9aefabc
Rohan Shah Summers <rohan_shah_summers@yahoo.co.uk>
2019-05-17 12:42:39 +0100

    generate cookie for filters and send forward to next page done relates #6 #31

---
+++ public/js/domFilter.js
@@ -0,0 +1,20 @@
+const tick = document.querySelector('.button__forward');
+const customerOnly = document.querySelector('#checkbox__customerOnly');
+const baby = document.querySelector('#checkbox__baby');
+const accessible = document.querySelector('#checkbox__accessible');
+const free = document.querySelector('#checkbox__free');
+const genderNeutral = document.querySelector('#checkbox__genderNeutral');
+const error = document.querySelector('.filter__error');
+
+const filterConfirm = () => {
+  const url = `/confirm?cus=${customerOnly.checked}&bab=${baby.checked}&acc=${accessible.checked}&fre=${free.checked}&gen=${genderNeutral.checked}`;
+  fetch(url)
+    .then(() => {
+      window.location.href = '/list';
+    })
+    .catch((err) => {
+      error.textContent = 'Oh No. Something went wrong.'
+    });
+};
+
+tick.addEventListener('click', filterConfirm);
