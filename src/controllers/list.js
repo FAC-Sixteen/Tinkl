b71af92511ac84cbf55e20d3e7d42bdb184ff573
Rohan Shah Summers <rohan_shah_summers@yahoo.co.uk>
2019-05-21 17:23:55 +0100

    add styling for list page, and back button on location page

--- src/controllers/list.js
+++ src/controllers/list.js
@@ -15,5 +15,94 @@ const listItemArray = [
     baby_changing: true,
     price: 0,
     gender_neutral: true,
+    map_link: 'https://assets.change.org/photos/5/sw/sy/JySWsYeBDXQvWna-800x450-noPad.jpg?1551704785'
+  },
+  {
+    name: 'Waterstones',
+    distance: '10 metres away',
+    customer_toilet: false,
+    address: '2-4 The Broadway, Crouch End, London N8 9SN',
+    accessible: true,
+    baby_changing: true,
+    price: 0,
+    gender_neutral: true,
+    map_link: 'https://assets.change.org/photos/5/sw/sy/JySWsYeBDXQvWna-800x450-noPad.jpg?1551704785'
+  },
+  {
+    name: 'Waterstones',
+    distance: '10 metres away',
+    customer_toilet: true,
+    address: '2-4 The Broadway, Crouch End, London N8 9SN',
+    accessible: true,
+    baby_changing: true,
+    price: 0,
+    gender_neutral: true,
+    map_link: 'https://assets.change.org/photos/5/sw/sy/JySWsYeBDXQvWna-800x450-noPad.jpg?1551704785'
+  },
+  {
+    name: 'Waterstones',
+    distance: '10 metres away',
+    customer_toilet: true,
+    address: '2-4 The Broadway, Crouch End, London N8 9SN',
+    accessible: true,
+    baby_changing: true,
+    price: 0,
+    gender_neutral: true,
+    map_link: 'https://assets.change.org/photos/5/sw/sy/JySWsYeBDXQvWna-800x450-noPad.jpg?1551704785'
+  },
+  {
+    name: 'Waterstones',
+    distance: '10 metres away',
+    customer_toilet: true,
+    address: '2-4 The Broadway, Crouch End, London N8 9SN',
+    accessible: true,
+    baby_changing: true,
+    price: 0,
+    gender_neutral: true,
+    map_link: 'https://assets.change.org/photos/5/sw/sy/JySWsYeBDXQvWna-800x450-noPad.jpg?1551704785'
+  },
+  {
+    name: 'Abercombe Council',
+    distance: '10 metres away',
+    customer_toilet: true,
+    address: '2-4 The Broadway, Crouch End, London N8 9SN',
+    accessible: true,
+    baby_changing: true,
+    price: 0,
+    gender_neutral: true,
+    map_link: 'https://assets.change.org/photos/5/sw/sy/JySWsYeBDXQvWna-800x450-noPad.jpg?1551704785'
+  },
+  {
+    name: 'Waterstones',
+    distance: '10 metres away',
+    customer_toilet: false,
+    address: '2-4 The Broadway, Crouch End, London N8 9SN',
+    accessible: true,
+    baby_changing: true,
+    price: 0,
+    gender_neutral: true,
+    map_link: 'https://assets.change.org/photos/5/sw/sy/JySWsYeBDXQvWna-800x450-noPad.jpg?1551704785'
+  },
+  {
+    name: 'Pret',
+    distance: '10 metres away',
+    customer_toilet: true,
+    address: '2-4 The Broadway, Crouch End, London N8 9SN',
+    accessible: true,
+    baby_changing: true,
+    price: 0,
+    gender_neutral: true,
+    map_link: 'https://assets.change.org/photos/5/sw/sy/JySWsYeBDXQvWna-800x450-noPad.jpg?1551704785'
+  },
+  {
+    name: 'Toilet',
+    distance: '10 metres away',
+    customer_toilet: true,
+    address: '2-4 The Broadway, Crouch End, London N8 9SN',
+    accessible: true,
+    baby_changing: true,
+    price: 0,
+    gender_neutral: true,
+    map_link: 'https://assets.change.org/photos/5/sw/sy/JySWsYeBDXQvWna-800x450-noPad.jpg?1551704785'
   },
 ];
