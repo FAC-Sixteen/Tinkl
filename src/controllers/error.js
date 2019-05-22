86b35eba05141c20a958e6fb771a27bf2a867b48
tloth <toth.annar@gmail.com>
2019-05-22 11:54:00 +0100

    create error pages (hbs and controllers), start CSS

---
+++ src/controllers/error.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+exports.client = (req, res) => {
+    res.status(404).render('error', {
+        layout: 'error',
+        statusCode: 404,
+        errorMessage: 'Urine trouble! No toilets here.'
+    });
+};
+
+exports.server = (err, req, res, next) => {
+    res.status(500).render('error', {
+        layout: 'error',
+        statusCode: 500,
+        errorMessage: 'Oops, there has been a little accident...'
+    })
+}
--- src/controllers/index.js
+++ src/controllers/index.js
@@ -10,6 +10,7 @@ const list = require('./list');
 const geolocation = require('./geolocation');
 const postcode = require('./postcode');
 const confirm = require('./confirm');
+const error = require('./error');
 
 router.get('/', home.get);
 router.get('/location', location.get);
@@ -18,5 +19,7 @@ router.get('/list', list.get);
 router.get('/geolocation', geolocation.get);
 router.get('/postcode', postcode.get);
 router.get('/confirm', confirm.get);
+router.use(error.client);
+router.use(error.server);
 
 module.exports = router;
