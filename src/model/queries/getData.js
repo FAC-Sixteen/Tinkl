06d0d8b5fafac50a20b3bd563cf871a546b3bbf9
tloth <toth.annar@gmail.com>
2019-05-22 10:47:48 +0100

    add limit 12 to database query

--- src/model/queries/getData.js
+++ src/model/queries/getData.js
@@ -2,7 +2,7 @@ const dbConnection = require('../database/db_connection');
 
 const getToilets = (lat, long) => {
     return new Promise((resolve, reject) => {
-        dbConnection.query('SELECT * FROM (SELECT *, ( 3959 * acos(cos(radians($1)) * cos(radians(latitude)) * cos(radians(longitude) - radians($2)) + sin(radians($1)) * sin(radians(latitude)))) AS distance FROM toilets) as distance WHERE distance < 1 ORDER BY distance;', [lat, long])
+        dbConnection.query('SELECT * FROM (SELECT *, ( 3959 * acos(cos(radians($1)) * cos(radians(latitude)) * cos(radians(longitude) - radians($2)) + sin(radians($1)) * sin(radians(latitude)))) AS distance FROM toilets) as distance WHERE distance < 1 ORDER BY distance LIMIT 12;', [lat, long])
         .then(toiletsClose => resolve(toiletsClose.rows))
         .catch(error => console.error(error));
     });
