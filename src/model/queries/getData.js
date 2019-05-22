0e6e082fb8709657e4b27061e2067eeb820a304a
Fweddi <40991816+Fweddi@users.noreply.github.com>
2019-05-22 12:37:56 +0100

    Update getData.js

--- src/model/queries/getData.js
+++ src/model/queries/getData.js
@@ -16,7 +16,7 @@ const getToilets = (lat, long, filters) => {
             SQLfilters = SQLfiltersArray.join('');
         }
 
-        const SQLquery = `SELECT * FROM (SELECT *, ( 3959 * acos(cos(radians($1)) * cos(radians(latitude)) * cos(radians(longitude) - radians($2)) + sin(radians($1)) * sin(radians(latitude)))) AS distance FROM toilets) as distance WHERE distance < 1 ${SQLfilters} ORDER BY distance LIMIT 12;`
+        const SQLquery = `SELECT * FROM (SELECT *, ( 3959 * acos(cos(radians($1)) * cos(radians(latitude)) * cos(radians(longitude) - radians($2)) + sin(radians($1)) * sin(radians(latitude)))) AS distance FROM toilets) AS distance WHERE distance < 1 ${SQLfilters} ORDER BY distance LIMIT 12;`
 
         dbConnection.query(SQLquery, [lat, long])
         .then(toiletsClose => resolve(toiletsClose.rows))
